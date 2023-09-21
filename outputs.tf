98e9f34a6211413657052623cb5337ff47956e31
Bogdan Ćulibrk <bogdan.culibrk@providus.rs>
2023-09-21 09:17:38 +0200

    PKE output rke_cluster_yaml

--- outputs.tf
+++ outputs.tf
@@ -14,18 +14,23 @@ output "api_server_url" {
   value = rke_cluster.pke.api_server_url
 }
 output "ca_crt" {
-  value = rke_cluster.pke.ca_crt
+  value     = rke_cluster.pke.ca_crt
   sensitive = true
 }
 output "client_key" {
-  value = rke_cluster.pke.client_key
+  value     = rke_cluster.pke.client_key
   sensitive = true
 }
 output "client_cert" {
-  value = rke_cluster.pke.client_cert
+  value     = rke_cluster.pke.client_cert
   sensitive = true
 }
 output "admin_user" {
-  value = rke_cluster.pke.kube_admin_user
+  value     = rke_cluster.pke.kube_admin_user
+  sensitive = true
+}
+
+output "rke_cluster_yaml" {
+  value     = rke_cluster.pke.rke_cluster_yaml
   sensitive = true
 }
